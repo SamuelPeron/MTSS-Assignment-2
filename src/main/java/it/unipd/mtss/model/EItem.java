69b74c13e76305066d6cda1442e23fb5161feec2
Mattia <mattia.brunello@studenti.unipd.it>
2022-05-21 11:29:04 +0200

    Test configurazione iniziale

--- src/main/java/it/unipd/mtss/model/EItem.java
+++ src/main/java/it/unipd/mtss/model/EItem.java
@@ -6,5 +6,35 @@
 package it.unipd.mtss.model;
 
 public class EItem {
+    public enum itemType {
+        PROCESSOR, MOTHERBOARD, MOUSE, KEYBOARD
+    }
 
+    itemType type;
+    Double price;
+    String name;
+
+    public EItem(itemType tipo, double prezzo, String nome) {
+        type = tipo;
+        price = prezzo;
+        name = nome;
+        if (price >= 0.0D) {
+            this.price = prezzo;
+        } else {
+            throw new IllegalArgumentException("Il prezzo deve essere maggiore 0");
+        }
+    }
+
+    public itemType getType() {
+        return type;
+    }
+
+    public double getPrice() {
+
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/it/unipd/mtss/business/BillImplementationTest.java
+++ src/test/java/it/unipd/mtss/business/BillImplementationTest.java
@@ -5,6 +5,24 @@
 
 package it.unipd.mtss.business;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.User;
+
 public class BillImplementationTest {
+    public List<EItem> itemsOrdered;
+    public BillImplementation bill;
+    public User user;
+
+    @Before
+    public void setUp() throws Exception {
+        bill = new BillImplementation();
+        user = new User("Mattia", "Brunello", LocalDate.of(2001, 12, 21), "mattia@email.com");
+        itemsOrdered = new ArrayList<EItem>();
+    }
 
 }
